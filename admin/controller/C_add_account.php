1ab6fdaddea651d39781879d9b6336cce82d4109
hoangngoc-lan <90963233+hoangngoc-lan@users.noreply.github.com>
2021-10-30 17:46:46 +0700

    sua file C_add_template

--- admin/controller/C_add_account.php
+++ admin/controller/C_add_account.php
@@ -2,17 +2,17 @@
 if(isset($_POST['btn_add'])){
 	$email_phone = $_POST['email_phone'];
 	$password=$_POST['password'];
-  
+
 
 	
 	$data_add=$db->get('account',array());
 	
  
 		$db->insert('account',array(
-			
+		
 			'email_phone'=>$email_phone,
-			'password'=>$password
-			
+			'password'=>$password,
+			'role'=>'user'
 			
 		));
 		// code...
