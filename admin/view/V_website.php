0d2b2ee31968696e17e7cbbb281b4fb3768bbb7e
VuThiMo1951060869 <dodaihoc64@gmail.com>
2021-11-02 18:48:17 +0700

    sua file V_website.php

--- admin/view/V_website.php
+++ admin/view/V_website.php
@@ -9,16 +9,23 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/website.css">
     <title></title>
+
 </head>
+
 <body>
 
     <br><br><br><br><br><br>
 
  <form  action="?controller=website" enctype="multipart/form-data" method="POST" >
-<h3>chọn hình ảnh</h3>
-<input type="file" name="upload_file">
-  <input type="submit" value="Upload " name="submit">
+<h3> Viết blog</h3>
+<!-- <input  type="text-area" style=" margin-left: 50%;" placeholder="bạn đang nghĩ gì "> -->
+<textarea id="tutorial" name="tutorial" rows="5" cols="40"  placeholder="bạn đang nghĩ gì "></textarea>
+<br>
+<input type="file" name="upload_file" >
+  <input type="submit" value="Upload " name="submit" style=" background-color: rgb(0, 81, 255); color: cornsilk; ">
+  
 
 
 
