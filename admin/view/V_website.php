181a191cb720240b19b665db689329848a7c623b
hoangngoc-lan <90963233+hoangngoc-lan@users.noreply.github.com>
2021-11-02 22:57:17 +0700

    them tinh nang upload anh va hien thi website

--- admin/view/V_website.php
+++ admin/view/V_website.php
@@ -18,12 +18,13 @@
 
     <br><br><br><br><br><br>
 
- <form  action="?controller=website" enctype="multipart/form-data" method="POST" >
+ <form  action="?controller=website" enctype="multipart/form-data" method="POST"  >
 <h3> Viết blog</h3>
 <!-- <input  type="text-area" style=" margin-left: 50%;" placeholder="bạn đang nghĩ gì "> -->
 <textarea id="tutorial" name="tutorial" rows="5" cols="40"  placeholder="bạn đang nghĩ gì "></textarea>
 <br>
 <input type="file" name="upload_file" >
+
   <input type="submit" value="Upload " name="submit" style=" background-color: rgb(0, 81, 255); color: cornsilk; ">
   
 
